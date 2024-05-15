c803c4c9e36a413e05175e05019662b0dcdab01c
Oliver Spacey <oliver.spacey@gmail.com>
2024-05-15 16:28:36 +0100

    Got service workers functioning. Full offline navigation is now possible. Attempting (hopefully) final offline functionality now - idb and pwa.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -17,7 +17,7 @@ let map;
 
 if ('serviceWorker' in navigator && 'SyncManager' in window) {
     navigator.serviceWorker.register('/sw.js').then((registration) => {
-        console.log('Service worker registered:', registration);
+        console.log('Service worker registered:', registration.scope);
     }).catch((error) => {
         console.error('Service worker registration failed:', error);
     });
